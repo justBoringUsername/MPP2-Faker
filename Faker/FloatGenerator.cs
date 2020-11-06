5c5bc79e9e551c2b064858ff0087cae231b96ffd
justBoringUsername <63564366+justBoringUsername@users.noreply.github.com>
2020-11-06 18:21:47 +0300

    Add files via upload

---
+++ Faker/FloatGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Faker;
+
+namespace Generators
+{
+    public class FloatGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate()
+        {
+            var mantissa = (Random.NextDouble() * 2.0) - 1.0;
+            var exponent = Math.Pow(2.0, Random.Next(-126, 128));
+            return (float)(mantissa * exponent);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(float);
+        }
+    }
+}
